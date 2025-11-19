7d4b96422a72239a8107890fd7962da7290d92ec
KAriNe.exe <poomzaapoom@gmail.com>
2025-11-19 15:50:32 +0700

    add answers

--- task02.ts
+++ task02.ts
@@ -1,5 +1,14 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = 42;
 // Try changing to "hello", 10, null, NaN
+let isNumber = typeof userInput;
+
+if (isNumber === "number")
+{
+    console.log(`T :${userInput} "This is Number"`);
+} 
+else {
+    console.log(`F :${userInput} "This is not a Number"`);
+}
